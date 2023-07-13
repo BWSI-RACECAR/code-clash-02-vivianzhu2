cbff5416069d3006530d1943680918d19ea2b6b4
Vivian Zhu <vivian.yijia.zhu@gmail.com>
2023-07-13 06:22:36 -0700

    Test2

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -2,24 +2,29 @@ class Solution:
     def digitdecrypt(self, num):
             #type num: int
             #return type: int
-            temp=0
-            digitlist=[]
-            i=0
-            #TODO: Write code below   to returnn an int with the solution to the prompt.
-            while(num%10!=0):
-                digitlist[i]= num%10
-                num/=10
-                i+=1
-            sum=0
-            digitlist=digitlist[::-1]
-            print(digitlist)
-            for i in range(0,digitlist-1):
-                 sum+= digitlist[i]
-            if(sum>10):
-                 digitdecrypt(sum)
-            else:
-                 return sum
- 
+            # temp=0
+            # digitlist=[]
+            # i=0
+            # #TODO: Write code below   to returnn an int with the solution to the prompt.
+            # while(num%10!=0):
+            #     digitlist[i]= num%10
+            #     num/=10
+            #     i+=1
+            # sum=0
+            # digitlist=digitlist[::-1]
+            # print(digitlist)
+            # for i in range(0,digitlist-1):
+            #      sum+= digitlist[i]
+            # if(sum>10):
+            #      digitdecrypt(sum)
+            # else:
+            #      return sum
+            while(num>0):
+                dig_list= [int[i]for i in str(num)]
+                num=0
+                for i in avg_list:
+                  num=num+1
+            return num
 def main():
     input1= input()
     input2 = int(input1)
